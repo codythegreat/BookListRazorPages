e4e8875d1308a9e97e611633bf9af900f46e0ed8
Cody Maxie <38113810+codythegreat@users.noreply.github.com>
2020-03-15 17:41:34 -0500

    Add all pertinent files to repo

---
+++ Pages/BookList/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class DeleteModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(Book.Id);
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ wwwroot/js/bookList.js
@@ -0,0 +1,83 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_Load').DataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "30%" },
+            { "data": "author", "width": "30%" },
+            { "data": "isbn", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="text-center">
+                        <a href="/Booklist/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px; margin-bottom:5px;">
+                            Edit
+                        </a>
+                        <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;"
+                            onclick="fakeDelete()">
+                            Delete
+                        </a>
+                    </div>`
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    })
+}
+
+
+// Delete('/api/book?id='+${data}) <== use this to delete entries via this function
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
+function fakeDelete() {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+
+        if (willDelete) {
+            toastr.success("You've fake deleted this book");
+        } else {
+            toastr.error("This book could not be deleted");
+        }
+    });
+}
